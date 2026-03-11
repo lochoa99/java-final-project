2281ffe74e07a807149e591c331d57c3d239cd86
l.ochoa <l.ochoa@globant.com>
2026-03-11 18:37:42 -0500

    Add hasStudent method to UniversityClass

--- src/models/UniversityClass.java
+++ src/models/UniversityClass.java
@@ -6,27 +6,34 @@ public class UniversityClass {
     private String classroom;
     private Teacher teacher;
     private ArrayList<Student> students;
-
-
     /**
     * @param name
     * @param classroom
     * @param teacher
     * @param students
     */
-
     public UniversityClass(String name, String classroom, Teacher teacher, ArrayList<Student> students) {
         this.name = name;
         this.classroom = classroom;
         this.teacher = teacher;
         this.students = new ArrayList<>(students);
     }
-
     /**
      * @param student
      */
     public void addStudent(Student student) {
         students.add(student);
+    }
+    /**
+     * @param studentId
+     */
+    public static hasStudent(int studentId) {
+        for (Student s : students) {
+            if (s.getId() == studentId) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
