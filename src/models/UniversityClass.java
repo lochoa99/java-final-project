79e6b014395f9d398b8d57b2257ca5091ff9b500
l.ochoa <l.ochoa@globant.com>
2026-03-11 16:38:39 -0500

    Add UniversityClass model with constructor and addStudent method

---
+++ src/models/UniversityClass.java
@@ -0,0 +1,33 @@
+package models;
+import java.util.ArrayList;
+
+public class UniversityClass {
+    private String name;
+    private String classroom;
+    private Teacher teacher;
+    private ArrayList<Student> students;
+
+
+    /**
+    * @param name
+    * @param classroom
+    * @param teacher
+    * @param students
+    */
+
+    public UniversityClass(String name, String classroom, Teacher teacher, ArrayList<Student> students) {
+        this.name = name;
+        this.classroom = classroom;
+        this.teacher = teacher;
+        this.students = new ArrayList<>(students);
+    }
+
+    /**
+     * @param student
+     */
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+
+}
