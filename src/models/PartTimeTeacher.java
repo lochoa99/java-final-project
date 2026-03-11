5ee3cc26b6fde71796612f438e79c715afd1029a
l.ochoa <l.ochoa@globant.com>
2026-03-11 18:59:54 -0500

    Add PartTimeTeacher class extending Teacher

--- src/models/PartTimeTeacher.java
+++ src/models/PartTimeTeacher.java
@@ -1,4 +1,10 @@
 package models;
 
-public class PartTimeTeacher {
+public class PartTimeTeacher extends Teacher {
+    private int activeHoursPerWeek;
+
+    public PartTimeTeacher(String name, String baseSalary, int activeHoursPerWeek) {
+        super(name, baseSalary);
+        this.activeHoursPerWeek = activeHoursPerWeek;
+    }
 }
