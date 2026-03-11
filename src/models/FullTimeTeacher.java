499107f7484a6de90401a3689630c340e6f53dfe
l.ochoa <l.ochoa@globant.com>
2026-03-11 18:50:24 -0500

    Add FullTimeTeacher class extending Teacher

--- src/models/FullTimeTeacher.java
+++ src/models/FullTimeTeacher.java
@@ -1,4 +1,12 @@
 package models;
+import java.text.DecimalFormat;
 
-public class FullTimeTeacher {
+public class FullTimeTeacher extends Teacher {
+    private int experienceYears;
+
+    public FullTimeTeacher(String name, double baseSalary, int experienceYears) {
+        super(name, baseSalary);
+        this.experienceYears = experienceYears;
+    }
+
 }
