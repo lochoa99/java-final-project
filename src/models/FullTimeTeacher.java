1ef756534e7d145035d11b2170e2da1606bbbe27
l.ochoa <l.ochoa@globant.com>
2026-03-11 18:56:39 -0500

    Add calculateSalary, getters, setters, toString to FullTimeTeacher

--- src/models/FullTimeTeacher.java
+++ src/models/FullTimeTeacher.java
@@ -4,9 +4,25 @@ import java.text.DecimalFormat;
 public class FullTimeTeacher extends Teacher {
     private int experienceYears;
 
-    public FullTimeTeacher(String name, double baseSalary, int experienceYears) {
+    public FullTimeTeacher(String name, String baseSalary, int experienceYears) {
         super(name, baseSalary);
         this.experienceYears = experienceYears;
     }
 
+    @Override
+    public double calculateSalary() {
+        return Double.parseDouble(getBaseSalary()) * 1.10 * experienceYears;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+    @Override
+    public String toString() {
+        return "[Full Time Teacher] " + super.toString() + ", Experience Years: " + experienceYears + " years"
+                + ", Calculated Salary: $" + String.format("%.2f", calculateSalary());
+    }
 }
