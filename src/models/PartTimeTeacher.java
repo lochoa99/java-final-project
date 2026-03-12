1826fcdeee8d85a53d25a3d81575770c59981a14
l.ochoa <l.ochoa@globant.com>
2026-03-11 19:03:48 -0500

    Add calculateSalary, getters, setters, toString to PartTimeTeacher

--- src/models/PartTimeTeacher.java
+++ src/models/PartTimeTeacher.java
@@ -7,4 +7,22 @@ public class PartTimeTeacher extends Teacher {
         super(name, baseSalary);
         this.activeHoursPerWeek = activeHoursPerWeek;
     }
+
+    @Override
+    public double calculateSalary() {
+        return Double.parseDouble(getBaseSalary()) * activeHoursPerWeek * 52;
+    }
+
+    public int getActiveHoursPerWeek() {
+        return activeHoursPerWeek;
+    }
+    public void setActiveHoursPerWeek(int activeHoursPerWeek) {
+        this.activeHoursPerWeek = activeHoursPerWeek;
+    }
+    @Override
+    public String toString() {
+        return "[Part Time Teacher] " + super.toString() +
+                ", Active Hours Per Week: " + activeHoursPerWeek + " hours"
+                + ", Calculated Salary: $" + String.format("%.2f", calculateSalary());
+    }
 }
