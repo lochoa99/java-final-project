9f4b89a0db0ae20432c201d5808f5a97ae1f5396
l.ochoa <l.ochoa@globant.com>
2026-03-11 19:27:03 -0500

    Add UniversityManager constructor and fields

--- src/services/UniversityManager.java
+++ src/services/UniversityManager.java
@@ -1,4 +1,21 @@
 package services;
+import models.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UniversityManager {
+
+    private ArrayList<UniversityClass> classes;
+    private ArrayList<Teacher> teachers;
+    private ArrayList<Student> students;
+    private Scanner scanner;
+
+    public UniversityManager(ArrayList<UniversityClass> classes,
+                             ArrayList<Teacher> teachers,
+                             ArrayList<Student> students) {
+        this.classes = classes;
+        this.teachers = teachers;
+        this.students = students;
+        this.scanner = new Scanner(System.in);
+    }
 }
