8c9053154ca671a1ff55045aabce804327f774d1
l.ochoa <l.ochoa@globant.com>
2026-03-11 19:07:37 -0500

    Add Student class with constructor and fields

--- src/models/Student.java
+++ src/models/Student.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Student {
+    private static int studentCount = 0;
+    private String name;
+    private int id;
+    private int age;
+
+    public Student(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        studentCount++;
+    }
 }
