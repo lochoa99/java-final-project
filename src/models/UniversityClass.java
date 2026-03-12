df411752d6bea0c7c34b1068b48263c968e0e507
l.ochoa <l.ochoa@globant.com>
2026-03-11 19:23:08 -0500

    Add getters, setters, and toString to UniversityClass

--- src/models/UniversityClass.java
+++ src/models/UniversityClass.java
@@ -21,7 +21,7 @@ public class UniversityClass {
         students.add(student);
     }
 
-    public static hasStudent(int studentId) {
+    public boolean hasStudent(int studentId) {
         for (Student s : students) {
             if (s.getId() == studentId) {
                 return true;
@@ -29,6 +29,41 @@ public class UniversityClass {
         }
         return false;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getClassroom() {
+        return classroom;
+    }
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+    public Teacher getTeacher() {
+        return teacher;
+    }
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Class ").append(name)
+                .append("\n | Classroom: ").append(classroom)
+                .append("\n | Teacher: ").append(teacher)
+                .append("\n | Students: ").append(students.size()).append("):");
+        for (Student s : students) {
+            sb.append("\n   - ").append(s.toString());
+        }
+        return sb.toString();
+    }
 
 }
